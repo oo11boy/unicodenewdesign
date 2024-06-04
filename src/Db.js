0ac8c2d82c5e9f52b800931f8f0ce75d3a181fd4
rasoul <rasoul137879@gmail.com>
2024-06-04 13:30:48 +0430

    demos created

--- src/Db.js
+++ src/Db.js
@@ -26,62 +26,361 @@ export const goodprice_design = {
     "سفارش طراحی سایت رزومه و اقتصادی به معنای درخواست طراحی وبسایتی است که هم برای ارائه رزومه شخصی یا حرفه‌ای کاربرد دارد و هم از لحاظ هزینه‌ها و بهره‌وری اقتصادی بهینه باشد. چنین سایتی معمولاً شامل بخش‌هایی برای معرفی سوابق تحصیلی، تجربیات کاری، مهارت‌ها و نمونه‌کارها می‌باشد و با استفاده از قالب‌های ساده و کارآمد، با هزینه کمتری طراحی و پیاده‌سازی می‌شود. هدف از این طراحی، ایجاد یک بستر حرفه‌ای و مقرون به صرفه برای معرفی توانمندی‌ها و جذب فرصت‌های شغلی یا تجاری است.",
 };
 
+export const AllDemos = [
+  {
+    name: "store_design",
+    Info: [
+      {
+        id: 1,
+        name: "دمو دیجیکالا",
+        link: "https://iran-woodmart.ir/demo/digikala/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp",
+      },
+      {
+        id: 2,
+        name: "فروشگاه مو تن رو ",
+        link: "https://iran-woodmart.ir/mootanro/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2023/10/mootanro.webp",
+      },
+      {
+        id: 3,
+        name: "دمو فروشگاه فایل",
+        link: "https://iran-woodmart.ir/demo/filestore/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/11/woodmart-filestore-demo.png",
+      },
+      {
+        id: 4,
+        name: "دمو فروش جواهرات",
+        link: "https://iran-woodmart.ir/demo/rosegold/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2024/03/woodmart-rosegold-demo.webp",
+      },
+      {
+        id: 5,
+        name: "دمو شیک پوش",
+        link: "https://iran-woodmart.ir/demo/shikposh",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2024/05/woodmart-demo-shikposh.webp",
+      },
+      {
+        id: 6,
+        name: "دمو فروشگاه عسل",
+        link: "https://iran-woodmart.ir/demo/asalmarket/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/09/asalmarket-woodmart-demo.png",
+      },
 
+      {
+        id: 7,
+        name: "دمو فروشگاه لوازم آرایشی",
+        link: "https://iran-woodmart.ir/demo/arayeshi/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2023/02/arayeshi-demo.png",
+      },
 
+      {
+        id: 8,
+        name: "دمو فروشگاه ساعت",
+        link: "https://iran-woodmart.ir/demo/saatshop/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/09/saatshop-woodmart-demo.png",
+      },
 
-  
+      {
+        id: 9,
+        name: "دمو فروشگاه لامپ و روشنایی",
+        link: "https://iran-woodmart.ir/demo/noorshop/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/09/noorshop-demo.png",
+      },
+      {
+        id: 10,
+        name: "دمو فروشگاه سخت افزار تیک کالا",
+        link: "https://iran-woodmart.ir/demo/tikakala/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/05/woodmart-theme-tikakala-demo.png",
+      },
+      {
+        id: 11,
+        name: "دمو فروشگاه موبایل و لوازم جانبی",
+        link: "https://iran-woodmart.ir/demo/lavazem-janebi/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/08/woodmart-theme-lavazem-janebi-demo.png",
+      },
+      {
+        id: 12,
+        name: "دمو فروشگاه عطر و ادکلن",
+        link: "https://iran-woodmart.ir/demo/perfume/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/05/woodmart-theme-perfume-demo.png",
+      },
 
-export const AllDemos=[
-    {
-        name:"store_design",
-        Info:[
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:2,name:"فروشگاه مو تن رو " ,link:"https://iran-woodmart.ir/mootanro/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/mootanro.webp"},
-            {id:3,name:"دمو فروشگاه فایل" ,link:"https://iran-woodmart.ir/demo/filestore/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/11/woodmart-filestore-demo.png"},
-            {id:4,name:"دمو فروش جواهرات" ,link:"https://iran-woodmart.ir/demo/rosegold/",img:"https://iran-woodmart.ir/wp-content/uploads/2024/03/woodmart-rosegold-demo.webp"},
-            {id:5,name:"دمو شیک پوش" ,link:"https://iran-woodmart.ir/demo/shikposh",img:"https://iran-woodmart.ir/wp-content/uploads/2024/05/woodmart-demo-shikposh.webp"},
-            {id:6,name:"دمو فروشگاه عسل" ,link:"https://iran-woodmart.ir/demo/asalmarket/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/09/asalmarket-woodmart-demo.png"},
-      
-            {id:7,name:"دمو فروشگاه لوازم آرایشی" ,link:"https://iran-woodmart.ir/demo/arayeshi/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/02/arayeshi-demo.png"},
-      
-            {id:8,name:"دمو فروشگاه ساعت" ,link:"https://iran-woodmart.ir/demo/saatshop/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/09/saatshop-woodmart-demo.png"},
-      
-            {id:9,name:"دمو فروشگاه لامپ و روشنایی" ,link:"https://iran-woodmart.ir/demo/noorshop/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/09/noorshop-demo.png"},
-            {id:10,name:"دمو فروشگاه سخت افزار تیک کالا" ,link:"https://iran-woodmart.ir/demo/tikakala/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/05/woodmart-theme-tikakala-demo.png"},
-            {id:11,name:"دمو فروشگاه موبایل و لوازم جانبی" ,link:"https://iran-woodmart.ir/demo/lavazem-janebi/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/08/woodmart-theme-lavazem-janebi-demo.png"},
-            {id:12,name:"دمو فروشگاه عطر و ادکلن" ,link:"https://iran-woodmart.ir/demo/perfume/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/05/woodmart-theme-perfume-demo.png"},
-      
-            {id:13,name:"دمو فروشگاه بازی - دیکاردو" ,link:"https://iran-woodmart.ir/demo/dicardo/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/04/woodmart-theme-dicardo-demo.png"},
-      
-            {id:14,name:"دمو فروشگاه لباس" ,link:"https://iran-woodmart.ir/demo/bootik/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/07/woodmart-theme-bootik-demo.png"},
-      
-            {id:15,name:"دمو فروشگاه دوره آموزشی" ,link:"https://iran-woodmart.ir/demo/learn/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/04/woodmart-theme-woodlearn-demo.png"},
-      
+      {
+        id: 13,
+        name: "دمو فروشگاه بازی - دیکاردو",
+        link: "https://iran-woodmart.ir/demo/dicardo/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/04/woodmart-theme-dicardo-demo.png",
+      },
 
-            {id:16,name:"دمو فروشگاه گل و گیاه" ,link:"https://iran-woodmart.ir/demo/woodplants/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/04/iran-woodmart-ir-woodplant.png"},
-            {id:17,name:"دمو فروشگاه جواهرات گلدشاپ" ,link:"https://iran-woodmart.ir/demo/goldshop/",img:"https://iran-woodmart.ir/wp-content/uploads/2022/07/woodmart-theme-goldshop-demo.png"},
-      
+      {
+        id: 14,
+        name: "دمو فروشگاه لباس",
+        link: "https://iran-woodmart.ir/demo/bootik/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/07/woodmart-theme-bootik-demo.png",
+      },
 
-        ]
-    },
-    {
-        name:"company_design",
-        Info:[
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-        ]
-    }
-    ,
-    {
-        name:"goodprice_design",
-        Info:[
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-            {id:1,name:"دمو دیجیکالا" ,link:"https://iran-woodmart.ir/demo/digikala/",img:"https://iran-woodmart.ir/wp-content/uploads/2023/10/digikala.webp"},
-        ]
-    }
-  
-]
+      {
+        id: 15,
+        name: "دمو فروشگاه دوره آموزشی",
+        link: "https://iran-woodmart.ir/demo/learn/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/04/woodmart-theme-woodlearn-demo.png",
+      },
+
+      {
+        id: 16,
+        name: "دمو فروشگاه گل و گیاه",
+        link: "https://iran-woodmart.ir/demo/woodplants/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/04/iran-woodmart-ir-woodplant.png",
+      },
+      {
+        id: 17,
+        name: "دمو فروشگاه جواهرات گلدشاپ",
+        link: "https://iran-woodmart.ir/demo/goldshop/",
+        img: "https://iran-woodmart.ir/wp-content/uploads/2022/07/woodmart-theme-goldshop-demo.png",
+      },
+    ],
+  },
+  {
+    name: "company_design",
+    Info: [
+      {
+        id: 1,
+        name: "دمو  شرکنی 1",
+        link: "https://wpshare.ir/zhaket/engitech/home-02/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-2.jpg",
+      },
+      {
+        id: 2,
+        name: "دمو شرکتی 2",
+        link: "https://wpshare.ir/zhaket/engitech/home-03/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-3.jpg",
+      },
+      {
+        id: 3,
+        name: "دمو شرکتی 3",
+        link: "https://wpshare.ir/zhaket/engitech/home-04/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-4.jpg",
+      },
+      {
+        id: 4,
+        name: "دمو شرکتی 4",
+        link: "https://wpshare.ir/zhaket/engitech/home-05/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-5.jpg",
+      },
+      ,
+      {
+        id: 5,
+        name: "دمو شرکتی 5",
+        link: "https://wpshare.ir/zhaket/engitech/home-06/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-6.jpg",
+      },
+      ,
+      {
+        id: 6,
+        name: "دمو شرکتی 6",
+        link: "https://wpshare.ir/zhaket/engitech/home-07/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-7.jpg",
+      },
+      ,
+      {
+        id: 7,
+        name: "دمو شرکتی 7",
+        link: "https://wpshare.ir/zhaket/engitech/home-08/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-8.jpg",
+      },
+      
+      {
+        id: 8,
+        name: "دمو شرکتی 8",
+        link: "https://wpshare.ir/zhaket/engitech/home-09/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-9.jpg",
+      },
+      
+      
+      {
+        id: 9,
+        name: "دمو شرکتی 9",
+        link: "https://wpshare.ir/zhaket/engitech/home-10/",
+        img: "https://wpshare.ir/zhaket/engitech-intro/Engitech_files/home-10.jpg",
+      },
+      {
+        id: 10,
+        name: "دمو شرکتی 10",
+        link: "https://demo.themsah.com/zeus/demo-1/",
+        img: "https://cdnsu.zhakethub.ir/public-smartupdater/installer-screenshots/6648a69d0cf9ab5be3040665/zhaket_665336a107eda.jpg",
+      },
+      {
+        id: 11,
+        name: "دمو شرکتی 11",
+        link: "https://wpshare.ir/zhaket/quantech/",
+        img: "https://cdnsu.zhakethub.ir/public-smartupdater/installer-screenshots/66267e15ecba6335b20a5409/zhaket_663b817e3e28e.jpg",
+      },
+      {
+        id: 12,
+        name: "دمو شرکتی 12",
+        link: "https://webteck.harmonicweb.ir/",
+        img: "https://cdnsu.zhakethub.ir/public-smartupdater/installer-screenshots/65d588914ab4e837aa0ac7d5/zhaket_65ed64a509936.jpg",
+      },
+      {
+        id: 13,
+        name: "دمو شرکتی 13",
+        link: "https://mediax.harmonicweb.ir/",
+        img: "https://mediax.harmonicweb.ir/landing/assets/img/pages/home-medical-clinic.jpg",
+      }
+      ,
+      {
+        id: 14,
+        name: "دمو شرکتی 14",
+        link: "https://mediax.harmonicweb.ir/dental-care/",
+        img: "https://mediax.harmonicweb.ir/landing/assets/img/pages/home-dental-care.jpg",
+      },
+
+      {
+        id: 15,
+        name: "دمو شرکتی 15",
+        link: "https://nioland.ir/d1/home-6/",
+        img: "https://nioland.ir/wp-content/uploads/2023/12/home-6-min.png",
+      },
+
+      {
+        id: 16,
+        name: "دمو شرکتی 16",
+        link: "https://nioland.ir/d1/home-5/",
+        img: "https://nioland.ir/wp-content/uploads/2023/12/home-5-min.png",
+      },
+      {
+        id: 17,
+        name: "دمو شرکتی 17",
+        link: "https://demo.wpsky.ir/robina/",
+        img: "https://demo.wpsky.ir/theme/robina-landing/wp-content/uploads/2023/12/FireShot-Capture-134-%D9%82%D8%A7%D9%84%D8%A8-%D8%B1%D9%88%D8%A8%DB%8C%D9%86%D8%A7-%D9%82%D8%A7%D9%84%D8%A8-%DA%86%D9%86%D8%AF%D9%85%D9%86%D8%B8%D9%88%D8%B1%D9%87-demo.wpsky_.ir_.png",
+      },
+
+      {
+        id: 18,
+        name: "دمو شرکتی 18",
+        link: "https://demo.wpsky.ir/robina/digital-demo/",
+        img: "https://demo.wpsky.ir/theme/robina-landing/wp-content/uploads/2023/12/FireShot-Capture-135-%D8%B5%D9%81%D8%AD%D9%87-%D8%A7%D8%B5%D9%84%DB%8C-%D8%AF%D9%85%D9%88%DB%8C-%D8%AF%DB%8C%D8%AC%DB%8C%D8%AA%D8%A7%D9%84-%D9%82%D8%A7%D9%84%D8%A8-%D8%B1%D9%88%D8%A8%DB%8C%D9%86%D8%A7-demo.wpsky_.ir_-1024x1020.png",
+      },
+      
+      {
+        id: 19,
+        name: "دمو شرکتی 19",
+        link: "https://demo.wpsky.ir/robina/business-demo/",
+        img: "https://demo.wpsky.ir/theme/robina-landing/wp-content/uploads/2023/12/FireShot-Capture-136-%D8%B5%D9%81%D8%AD%D9%87-%D8%A7%D8%B5%D9%84%DB%8C-%D8%AF%D9%85%D9%88%DB%8C-%DA%A9%D8%B3%D8%A8-%D9%88-%DA%A9%D8%A7%D8%B1-%D9%82%D8%A7%D9%84%D8%A8-%D8%B1%D9%88%D8%A8%DB%8C%D9%86%D8%A7-demo.wpsky_.ir_-1024x1020.png",
+      },
+      {
+        id: 20,
+        name: "دمو شرکتی 20",
+        link: "https://nioland.ir/d1/home-8/",
+        img: "https://nioland.ir/wp-content/uploads/2023/12/home-8-min.png",
+      },
+    ],
+  },
+  {
+    name: "goodprice_design",
+    Info: [
+      {
+        id: 1,
+        name: "دمو شخصی رزومه ای 1",
+        link: "https://coderwp.ir/dara/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/d20.jpg",
+      },
+      {
+        id: 2,
+        name: "دمو شخصی رزومه ای 2",
+        link: "https://coderwp.ir/academic-advice/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/01psr.png",
+      },
+      {
+        id: 3,
+        name: "دمو شخصی رزومه ای 3",
+        link: "https://coderwp.ir/lawyer/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/04psy.png",
+      },
+      {
+        id: 4,
+        name: "دمو شخصی رزومه ای 4",
+        link: "https://coderwp.ir/developer/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/dev201.png",
+      },
+      {
+        id: 5,
+        name: "دمو شخصی رزومه ای 5",
+        link: "https://coderwp.ir/ui/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/d020.png",
+      },
+      {
+        id: 6,
+        name: "دمو شخصی رزومه ای 6",
+        link: "https://coderwp.ir/seo/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/new-c.png",
+      },
+      
+      {
+        id: 7,
+        name: "دمو شخصی رزومه ای 7",
+        link: "https://coderwp.ir/influencer/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/new-alight.png",
+      },
+      {
+        id: 8,
+        name: "دمو شخصی رزومه ای 8",
+        link: "https://coderwp.ir/trader2/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/new-f.png",
+      },
+      {
+        id: 9,
+        name: "دمو شخصی رزومه ای 9",
+        link: "https://coderwp.ir/dress-designer/",
+        img: "https://dara2.s3.ir-thr-at1.arvanstorage.ir/ref546.png",
+      },
+      {
+        id: 10,
+        name: "دمو شخصی رزومه ای 10",
+        link: "https://ghaleblake.ir/wp_themes/savis/",
+        img: "https://ghaleblake.ir/landing/wp-content/uploads/2022/09/1.jpeg",
+      }
+      ,
+      {
+        id: 11,
+        name: "دمو شخصی رزومه ای 11",
+        link: "https://ghaleblake.ir/wp_themes/savis/home-2",
+        img: "https://ghaleblake.ir/landing/wp-content/uploads/2022/09/s2.jpeg",
+      }
+      ,
+      {
+        id: 12,
+        name: "دمو شخصی رزومه ای 12",
+        link: "https://ghaleblake.ir/wp_themes/savis/home-3",
+        img: "https://ghaleblake.ir/landing/wp-content/uploads/2023/04/s3-2-min.png",
+      }
+      ,
+      {
+        id: 13,
+        name: "دمو شخصی رزومه ای 13",
+        link: "https://ghaleblake.ir/wp_themes/savis/home-4",
+        img: "https://ghaleblake.ir/landing/wp-content/uploads/2022/09/savis-4.jpeg",
+      }
+      ,
+      {
+        id: 14,
+        name: "دمو شخصی رزومه ای 14",
+        link: "https://v3dboy.ir/previews/wordpress/cvup/%D8%B1%D9%86%DA%AF-%D8%A7%D8%B5%D9%84%DB%8C-%D8%B3%D8%A8%D8%B2",
+        img: "https://v3dboy.ir/previews/wordpress/cvup/wp-content/uploads/2023/02/screenshot_green.jpg",
+      },
+      {
+        id: 15,
+        name: "دمو شخصی رزومه ای 15",
+        link: "https://v3dboy.ir/previews/wordpress/procard/",
+        img: "https://files.rtl-theme.com/contents/post-112091/3bea7239d548d6d6cc166207c29905c831fd844cc51c6.png",
+      },
+      {
+        id: 16,
+        name: "دمو شخصی رزومه ای 16",
+        link: "https://v3dboy.ir/previews/wordpress/new/",
+        img: "https://files.rtl-theme.com/products/content/2018/08/21818_3fb0d444673a6620f983ffd91.jpg",
+      },
+    ],
+  },
+];
